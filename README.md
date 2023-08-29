d83198cb51e8076638cce681f44d187bf70f167c
Brett Schellenberg <brett.schellenberg@gmail.com>
2023-08-29 16:19:32 -0600

    logos and links

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This is a game where you try to create an ordered list of randomly generated numbers. Inspired by [this reddit post](https://www.reddit.com/r/theydidthemath/comments/164hq3h/request_what_are_the_odds/).
 
 
+## Live
+You can play here: [https://pabo.github.io/20numbers/]
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import "./App.css";
 import { Game } from "./Game";
+import githubImgUrl from "./assets/github-mark.png";
 
 function App() {
-  return <Game></Game>;
+  return (
+    <>
+      <div className="links">
+        <a href="https://github.com/pabo/20numbers">
+          <img src={githubImgUrl} height="30px" />
+        </a>
+      </div>
+      <Game />
+    </>
+  );
 }
 
 export default App;
